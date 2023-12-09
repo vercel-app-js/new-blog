d019dd7689f0fbaa23494b1d362c322ad4f2d5d0
vercel-app-js <142223815+vercel-app-js@users.noreply.github.com>
2023-12-09 21:13:17 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "wild",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "https://repo-main.wildmoney.pro/blog-data/avatar/notion-avatar-1702145421281.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "",
